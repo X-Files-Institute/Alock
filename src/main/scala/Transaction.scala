af1365a3a0b9d673db9479ebc4405229aec11097
Muqiu Han <muqiu-han@outlook.com>
2022-12-30 16:13:44 +0800

    INIT

---
+++ src/main/scala/Transaction.scala
@@ -0,0 +1,3 @@
+/// The transaction is a very simple object:
+/// it has a sender, a recipient and a value. We can implement it as a simple case class.
+case class Transaction(sender: String, recipient: String, value: Long)
